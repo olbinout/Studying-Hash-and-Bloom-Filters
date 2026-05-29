56bb17d1dcced7af57a90335b4856a0685dc541f
Otavio Linhares <testeotavio124@gmail.com>
2026-05-29 11:19:03 -0300

    (code) Implementação de função de busca

--- teste2.c
+++ teste2.c
@@ -19,6 +19,7 @@ typedef struct Hashmap{
 // Functions
 int hashFunction (Aluno X);
 void inserir(Hashmap *ht, Aluno X);
+No* buscar(Hashmap *ht, Aluno X);
 
 int main (){
 
@@ -37,6 +38,8 @@ int main (){
     printf("Índice da tabela Hash de %s: %d\n", A2.nome, hashFunction(A2));
 
     printf("Depois de %s é %s\n", A1.nome, ht->discentes[hashFunction(A1)]->prox->estudante.nome);
+
+    printf("A matrícula (chave) do aluno é: %d\n", buscar(ht, A1)->estudante.matricula);
 
     return 0;
 }
@@ -63,3 +66,16 @@ void inserir(Hashmap *ht, Aluno X){
         atual->prox = novoNo;
     }
 }
+
+No* buscar(Hashmap *ht, Aluno X){
+    int indice = hashFunction(X);
+    No *atual = ht->discentes[indice]; // Atual é um ponteiro que vai percorrer a lista encadeada
+    while (atual != NULL){
+        if (atual->estudante.matricula == X.matricula){
+            return atual; // Serve para retornar o endereço do aluno
+        }
+        atual = atual->prox; // Serve para percorrer a lista encadeada
+    }
+    return NULL; 
+}
+
